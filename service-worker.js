3f1fd134a577c317bbdd9e2ca2f9538ac6c13cf5
unknown <pronunciatio@naver.com>
2020-07-09 15:17:23 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/netflixclone/precache-manifest.5070b58d60e1804ae61eda2ed8292f26.js"
+  "/netflixclone/precache-manifest.2bc8171c552e4a0d4e136ea2865f40ad.js"
 );
 
 self.addEventListener('message', (event) => {
